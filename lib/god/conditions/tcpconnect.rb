44db3ef236b1f9b6f1b0726ce46bd27eacf60f4e
Dallas Marlow <dallas@tumblr.com>
2011-02-15 18:03:53 -0500

    added a tcpconnect condition

---
+++ lib/god/conditions/tcpconnect.rb
@@ -0,0 +1,37 @@
+module God
+  module Conditions
+    class Tcpconnect < PollCondition
+      attr_accessor :port
+                    :host,
+                    :timeout
+      def initialize
+        super
+        require 'socket'
+        require 'timeout'
+        self.host = '0.0.0.0'
+        self.timeout = 1.second
+      end
+
+      def valid?
+        valid = true
+        valid &= complain("Attribute 'port' must be specified", self) if self.port.nil?
+        valid
+      end
+
+      def test
+        begin
+          Timeout::timeout(self.timeout) do
+            begin
+              TCPSocket.new(self.host, self.port).close
+              return false
+            rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+            end
+          end
+        rescue Timeout::Error
+        end
+        return true
+      end
+
+    end
+  end
+end
